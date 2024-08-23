0862277ef732e40def4c2f96adb7fa415e5edb6c
Jmara <jmara.0923@gmail.com>
2024-08-23 17:31:42 +0000

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let multString = "";
 
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    multString += string;
+  }
+  return multString;
 };
 
 // Do not edit below this line
