7d7851fada7db438e7ea10ba09cc3ab7c82778cf
Jmara <jmara.0923@gmail.com>
2024-08-24 17:47:22 +0100

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...remove) {
+  for (const element of remove) {
+    console.log(element);
+    array = array.filter((value) => value !== element);
+  }
+
+  return array;
 };
 
 // Do not edit below this line
