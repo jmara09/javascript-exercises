8d22677d76bbfffb603461efc3bd9cdbc215e529
Jmara <jmara.0923@gmail.com>
2024-08-25 18:50:29 +0100

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (deg) {
+  let fahrenheit = ((deg - 32) * 5) / 9;
+
+  if (!Number.isInteger(fahrenheit)) {
+    return (fahrenheit = Number(fahrenheit.toFixed(1)));
+  } else {
+    return fahrenheit;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (deg) {
+  let celsius = (deg * 9) / 5 + 32;
+
+  if (!Number.isInteger(celsius)) {
+    return (celsius = Number(celsius.toFixed(1)));
+  } else {
+    return celsius;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
