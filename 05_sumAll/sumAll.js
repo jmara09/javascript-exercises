c8fe538ca27256aa41ff5f45d5999bae4b9e355b
Jmara <jmara.0923@gmail.com>
2024-08-25 02:25:50 +0100

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  const numArray = [num1, num2];
 
+  for (let i = 0; i <= numArray.length - 1; i++) {
+    if (isNaN(numArray[i])) {
+      return "ERROR";
+    } else if (!Number.isInteger(numArray[i]) || numArray[i] < 0) {
+      return "ERROR";
+    }
+  }
+
+  if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = num2; i <= num1; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
