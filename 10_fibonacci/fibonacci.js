dcd2816d1e5d61e4d8383be294670535b159990d
Jmara <jmara.0923@gmail.com>
2024-09-04 21:55:52 +0200

    Finish js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (parseInt(n) < 0) {
+    return "OOPS";
+  } else if (parseInt(n) == 0) {
+    return 0;
+  }
 
+  let arr = [];
+  let counter = parseInt(n);
+  let last = 0;
+
+  do {
+    if (arr.length < 2) {
+      arr.push(1);
+      counter--;
+    } else {
+      let lastValue = arr[arr.length - 1];
+      let secondToLast = arr[arr.length - 2];
+      last = secondToLast + lastValue;
+      arr.push(last);
+      counter--;
+    }
+  } while (counter > 0);
+
+  return arr[arr.length - 1];
 };
 
 // Do not edit below this line
