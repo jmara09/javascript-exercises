ad1b805a9d87f6a816464e3d4b9d316d8adf8c16
Jmara <jmara.0923@gmail.com>
2024-09-04 21:22:04 +0200

    Finish js exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let str = "";
+  for (let i = 0; i < string.length; i++) {
+    let code = string.charCodeAt(i);
+    if (
+      (code > 47 && code < 58) ||
+      (code > 64 && code < 91) ||
+      (code > 96 && code < 123)
+    ) {
+      str += string[i];
+    }
+  }
 
+  str = str.toLowerCase();
+  reversedStr = str.split("").reverse().join("");
+
+  if (str === reversedStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
