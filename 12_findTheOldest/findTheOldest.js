4804b871b20c141a2d9aeb79028479c87cece877
Jmara <jmara.0923@gmail.com>
2024-09-05 12:07:42 +0200

    Finish js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let oldPerson = arr
+    .map((person) => {
+      if (person.yearOfDeath == undefined) {
+        let current = new Date();
+        let currentYear = current.getFullYear();
 
+        let pers = {
+          name: person.name,
+          age: currentYear - person.yearOfBirth,
+        };
+        return pers;
+      } else {
+        let perss = {
+          name: person.name,
+          age: person.yearOfDeath - person.yearOfBirth,
+        };
+        return perss;
+      }
+    })
+    .sort((a, b) => b.age - a.age);
+
+  return oldPerson[0];
 };
 
 // Do not edit below this line
